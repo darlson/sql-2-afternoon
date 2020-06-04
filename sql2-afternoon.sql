392fbe1de90fc667d62df1849ae96240d9f45e40
darlson <davertiser@gmail.com>
2020-06-04 00:43:41 -0600

    completed Rows

--- sql2-afternoon.sql
+++ sql2-afternoon.sql
@@ -90,7 +90,28 @@ where album_id in (
 
 -- -- -- -- -- -- -- -- --
 -- -- Updating Rows- -- --
+update customer
+set fax = null
+where fax is not null
 
+update customer
+set company = 'Self'
+where company is null
+
+update customer
+set last_name = 'Thompson'
+where first_name = 'Julia' AND last_name = 'Barnett'
+
+update customer
+set support_rep_id = 4
+where email = 'luisrojas@yahoo.cl'
+
+update track
+set composer = 'The darkness around us'
+where composer is null AND genre_id = (
+  select genre_id from genre
+  where name = 'Metal'
+)
 
 -- -- -- -- -- -- -- -- --
 -- -- -- Group By -- -- --
